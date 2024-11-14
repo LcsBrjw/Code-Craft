5b644e8d655ac5089ca8ae7611bbf66376de17af
Sylvain <sylvain.feillault@gmail.com>
2024-11-14 15:30:10 +0100

    creation des overlay

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+// BURGER MENU
+
 const burgerButton = document.querySelector('#burger-menu');
+const burgerMenu = document.querySelector('#burger-nav');
 const topBar = document.querySelector('.top-bar');
 const midBar = document.querySelector('.middle-bar');
 const botBar = document.querySelector('.bottom-bar');
@@ -8,4 +11,32 @@ burgerButton.addEventListener('click', function() {
     topBar.classList.toggle('active')
     midBar.classList.toggle('active')
     botBar.classList.toggle('active')
+    burgerMenu.classList.toggle('active')
+    signLog.classList.remove('active')
+    logIn.classList.remove('active')
+})
+
+
+
+
+// CHOIX LOGIN SIGNIN
+const userImg = document.querySelector('#user-img');
+const signLog = document.querySelector('#sign-log');
+
+userImg.addEventListener('click', function() {
+    signLog.classList.toggle('active')
+
+})
+
+
+// GESTION LOGIN
+const logButt = document.querySelector('#login-button');
+const cancButt = document.querySelector('#annul');
+const logIn = document.querySelector('#login');
+
+logButt.addEventListener('click', function() {
+    logIn.classList.add('active')
+})
+cancButt.addEventListener('click', function() {
+    logIn.classList.remove('active')
 })
