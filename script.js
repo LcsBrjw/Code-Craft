77c4442d717d9bb4df0a6401a7f4ffa8bd193cc7
Sylvain <sylvain.feillault@gmail.com>
2024-11-15 18:18:55 +0100

    landing page en cours de finition

--- script.js
+++ script.js
@@ -1,34 +1,11 @@
-// BURGER MENU
-
-const burgerButton = document.querySelector('#burger-menu');
-const burgerMenu = document.querySelector('#burger-nav');
-const topBar = document.querySelector('.top-bar');
-const midBar = document.querySelector('.middle-bar');
-const botBar = document.querySelector('.bottom-bar');
-
-
-burgerButton.addEventListener('click', function() {
-    topBar.classList.toggle('active')
-    midBar.classList.toggle('active')
-    botBar.classList.toggle('active')
-    burgerMenu.classList.toggle('active')
-    signLog.classList.remove('active')
-    logIn.classList.remove('active')
-})
-
-
-
-
 // CHOIX LOGIN SIGNIN
 const userImg = document.querySelector('#user-img');
 const signLog = document.querySelector('#sign-log');
 
 userImg.addEventListener('click', function() {
     signLog.classList.toggle('active')
-    burgerMenu.classList.remove('active')
-    topBar.classList.remove('active')
-    midBar.classList.remove('active')
-    botBar.classList.remove('active')
+    logIn.classList.remove('active')
+
 })
 
 
@@ -39,7 +16,6 @@ const logIn = document.querySelector('#login');
 
 logButt.addEventListener('click', function() {
     logIn.classList.add('active')
-    burgerMenu.classList.remove('active')
 })
 cancButt.addEventListener('click', function() {
     logIn.classList.remove('active')
